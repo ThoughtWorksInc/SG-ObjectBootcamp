a7ca7a18976587559cc48a3633bf23520cfea617
Stephan Dowding <stephan.dowding@gmail.com>
2015-08-18 03:27:32 +0800

    OpenClosed

---
+++ OpenClosed/src/test/java/PriorityQueueTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+public class PriorityQueueTest {
+
+    @Test
+    public void addingAnItemIncreasesSize(){
+//        PriorityQueue priQ = new PriorityQueue();
+
+//        priQ.add(27);
+//        priQ.add(33);
+//        priQ.add(30);
+
+//        assertThat(priQ.size(), is(equalTo(3)));
+    }
+
+    @Test
+    public void itemsAreRemovedWhenTaken(){
+//        PriorityQueue priQ = new PriorityQueue();
+
+//        priQ.add(27);
+//        priQ.add(33);
+//        priQ.add(30);
+
+//        priQ.take();
+//        priQ.take();
+
+//        assertThat(priQ.size(), is(equalTo(1)));
+    }
+
+    @Test
+    public void itemsAreRemovedInAscendingOrder(){
+//        PriorityQueue priQ = new PriorityQueue();
+
+//        priQ.add(27);
+//        priQ.add(33);
+//        priQ.add(30);
+
+//        assertThat(priQ.take(), is(equalTo(27)));
+//        assertThat(priQ.take(), is(equalTo(30)));
+//        assertThat(priQ.take(), is(equalTo(33)));
+    }
+}
