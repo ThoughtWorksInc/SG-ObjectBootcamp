059456c18751a6a53c720ae2aa07333c771fc0ce
Thao Dang <tdang@thoughtworks.com>
2016-09-14 22:56:43 +0800

    BloppiDate built for eclipse

--- InheritancePitfalls/src/main/java/CountingList.java
+++ InheritancePitfalls/src/main/java/CountingList.java
@@ -27,44 +27,4 @@ public class CountingList extends MyStringList {
     public String toString() {
         return String.format("Added %d elements: %s", counter, super.concatenateList(", "));
     }
-
-    public class SideEffectVsImmutable {
-        public List<Integer> multiplyByTwoWithForLoop(List<Integer> list) {
-            for(int i = 0; i < list.size(); i++) {
-                int doubled = list.get(i) * 2;
-                list.set(i, doubled);
-            }
-            return list;
-        }
-
-        public List<Integer> multiplyByTwoWithStream(List<Integer> list) {
-            return list.stream().map((x) -> x * 2).collect(Collectors.toList());
-            //list.stream(): creates a stream from the list
-            //map(Function<Integer, Integer> f): map applies the given function f to transform every element in the stream
-            //collect(Collectors.toList()) the stream elements back into a list
-        }
-
-        public String transformToString(int x,
-                                        Function<Integer, Integer> transformFunction) {
-            int transformed = transformFunction.apply(x);
-            return String.valueOf(transformed);
-        }
-
-        public String doubleUp(int x) {
-            return transformToString(x, y -> y * 2);
-        }
-
-        public String tripleUp(int x) {
-            return transformToString(x, y -> y * 3);
-        }
-
-        public String toBase2String(int x) {
-            return transformToString(x, this::toBase2);
-        }
-
-        public int toBase2(int x) {
-            return x % 2;
-        }
-
-    }
 }
