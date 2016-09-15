9cf1fb0f2621cd3e2803598f96d84e070935bdf8
Stephan Dowding <stephan.dowding@gmail.com>
2016-09-15 21:55:56 +0800

    switching junit libs to used embedded eclipse version

--- InterfaceSegregation/test/DriverTest.java
+++ InterfaceSegregation/test/DriverTest.java
@@ -1,11 +1,7 @@
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 public class DriverTest {
     @Test
